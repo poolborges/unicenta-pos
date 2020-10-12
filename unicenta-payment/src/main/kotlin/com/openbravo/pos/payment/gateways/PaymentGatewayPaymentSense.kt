074986a1c7371fdbbe5bb9c23a0113629f3a0804
Paulo Borges <poolborges@hotmail.com>
2020-10-12 17:21:02 +0100

    Move PaymentGateways(Dejavoo, PaymentSense, Vantiv) to another artifact/project
    Artifact unicenta-payment (NOT Compiling yet, because need missing jar/library)
    
    Should get this jar from unicenta or will be completed removed. 
    
    * https://unicenta.com/
    * https:/posapps.io

---
+++ unicenta-payment/src/main/kotlin/com/openbravo/pos/payment/gateways/PaymentGatewayPaymentSense.kt
@@ -0,0 +1,40 @@
+package com.openbravo.pos.payment
+
+import uk.co.pos_apps.PosApps
+import uk.co.pos_apps.common.AppContext
+
+class PaymentGatewayPaymentSense :PaymentGateway {
+
+    val PAYMENT_PROCESSOR = "PaymentSense"
+
+    override fun execute(payinfo: PaymentInfoMagcard?) {
+
+        AppContext.paymentComplete = false
+        AppContext.paymentResult = null
+        AppContext.P_S_PDQ_STATUS = "Initializing..."
+
+        var timer = 0
+        val timeout = 180
+
+        PosApps.initPayment(PAYMENT_PROCESSOR, payinfo?.total)
+
+        while (!AppContext.paymentComplete) {
+            Thread.sleep(1000)
+            timer += 1
+            if (timer > timeout) break
+        }
+
+        if (AppContext.paymentResult == null) {
+            payinfo?.paymentError("Transaction Error! Please try again", "No Response")
+        }
+        if (AppContext.paymentResult.transactionResult == "SUCCESSFUL"){
+            payinfo?.cardName = AppContext.paymentResult.cardSchemeName
+            payinfo?.setVerification(AppContext.paymentResult.paymentMethod)
+            payinfo?.chipAndPin = true
+            payinfo?.paymentOK(AppContext.paymentResult.authCode, AppContext.paymentResult.transactionId, AppContext.paymentResult.transactionResult)
+        }
+        else {
+            payinfo?.paymentError("Transaction Error! Please try again", AppContext.paymentResult.transactionResult)
+        }
+    }
+}
