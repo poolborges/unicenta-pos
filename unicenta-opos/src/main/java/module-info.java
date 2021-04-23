d6090947fbb0bcb43f5b3ce7416930272eb2824a
Paulo Borges <poolborges@hotmail.com>
2021-04-23 20:28:22 +0100

    Initial support for Java Platform Module System (JPMS)
    * Added module-info.java
    * Solved compile time problems

---
+++ unicenta-opos/src/main/java/module-info.java
@@ -0,0 +1,82 @@
+//    KrOS POS  - Open Source Point Of Sale
+//    Copyright (c) 2009-2018 uniCenta & previous Openbravo POS works
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// future package 
+
+module io.github.kriolos.krpos {
+    
+    //requires ant;
+    //requires ant.launcher;
+    //requires avalon.framework.impl;
+    requires AbsoluteLayout.RELEASE121;
+    requires barcode4j;
+    requires bsh;
+    requires commons.cli;
+    requires commons.lang;
+    requires commons.collections;
+    requires castor.xml;
+    requires castor.core;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.annotation;
+    requires commons.lang3;
+    requires commons.beanutils;
+    requires commons.digester;
+    requires commons.logging;
+    requires com.formdev.flatlaf;
+    requires com.formdev.flatlaf.swingx;
+    requires ecj;
+    requires HdrHistogram;
+    requires itext;
+    requires javacsv;
+    requires javapos;
+    //requires javapos.contracts;
+    requires jdom2;
+    requires javatuples;
+    requires jline;
+    requires jasperreports;
+    requires jcommon;
+    requires jfreechart;
+    requires jasperreports.fonts;
+    requires java.persistence;
+    requires java.sql;
+    requires java.logging;
+    requires java.desktop;
+    requires java.rmi;
+    requires java.xml;
+    requires java.xml.bind;
+    requires java.se;
+    requires javax.inject;
+    requires jdatepicker;
+    requires libusb4java;
+    requires liquibase.core;
+    requires org.jpos.jpos;
+    requires org.apache.commons.codec;
+    requires org.bouncycastle.provider;
+    requires org.bouncycastle.pg;
+    //requires org.slf4j;
+    requires org.apache.commons.collections4;
+    //requires onewire.api;
+    requires rxtxcomm;
+    requires swingx.all;
+    requires swing.layout;
+    requires usb4java;
+    requires usb.api;
+    requires unicenta.opos.i18n;
+    requires thumbnailator;
+    requires velocity;
+    requires weblaf.complete;
+}
