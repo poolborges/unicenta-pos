529570e4ac2d120f16ccdf0a0d33e38689d28da5
Paulo Borges <poolborges@hotmail.com>
2021-04-26 01:04:18 +0100

    Update User

--- unicenta-opos/src/main/java/com/openbravo/data/user/FilterEditorCreator.java
+++ unicenta-opos/src/main/java/com/openbravo/data/user/FilterEditorCreator.java
@@ -23,10 +23,5 @@ import com.openbravo.data.loader.SerializerWrite;
  * @author adrian
  */
 public interface FilterEditorCreator extends EditorCreator {
-
-    /**
-     *
-     * @return
-     */
     public SerializerWrite getSerializerWrite();    
 }
