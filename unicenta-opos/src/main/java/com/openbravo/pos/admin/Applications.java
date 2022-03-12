0bb8b261b343ec790be791c72e2787ff4559aa00
Paulo Borges <'poolborges@hotmail.com'>
2022-03-12 14:57:55 -0100

    Move files

---
+++ unicenta-opos/src/main/java/com/openbravo/pos/admin/Applications.java
@@ -0,0 +1,132 @@
+//    KrOS POS  - Open Source Point Of Sale
+//    Copyright (c) 2009-2018 uniCenta & previous Openbravo POS works
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package com.openbravo.pos.admin;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+/**
+ *
+ * @author jack
+ */
+@Entity
+@Table(name = "applications", catalog = "unicentaopos", schema = "")
+@NamedQueries({
+    @NamedQuery(name = "Applications.findAll", query = "SELECT a FROM Applications a"),
+    @NamedQuery(name = "Applications.findById", query = "SELECT a FROM Applications a WHERE a.id = :id"),
+    @NamedQuery(name = "Applications.findByName", query = "SELECT a FROM Applications a WHERE a.name = :name"),
+    @NamedQuery(name = "Applications.findByVersion", query = "SELECT a FROM Applications a WHERE a.version = :version")})
+public class Applications implements Serializable {
+    @Transient
+    private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private String id;
+    @Basic(optional = false)
+    @Column(name = "NAME")
+    private String name;
+    @Basic(optional = false)
+    @Column(name = "VERSION")
+    private String version;
+
+    public Applications() {
+    }
+
+    public Applications(String id) {
+        this.id = id;
+    }
+
+    public Applications(String id, String name, String version) {
+        this.id = id;
+        this.name = name;
+        this.version = version;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        String oldId = this.id;
+        this.id = id;
+        changeSupport.firePropertyChange("id", oldId, id);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        String oldName = this.name;
+        this.name = name;
+        changeSupport.firePropertyChange("name", oldName, name);
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        String oldVersion = this.version;
+        this.version = version;
+        changeSupport.firePropertyChange("version", oldVersion, version);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Applications)) {
+            return false;
+        }
+        Applications other = (Applications) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.openbravo.pos.mant.Applications[ id=" + id + " ]";
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.removePropertyChangeListener(listener);
+    }
+    
+}
