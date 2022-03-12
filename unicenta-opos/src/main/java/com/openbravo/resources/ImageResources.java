2f8fcb942be85d69f21c0e42c6e14e8c2298e268
Paulo Borges <'poolborges@hotmail.com'>
2022-03-12 15:45:37 -0100

    License Headers

--- unicenta-opos/src/main/java/com/openbravo/resources/ImageResources.java
+++ unicenta-opos/src/main/java/com/openbravo/resources/ImageResources.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 KiolOS<https://github.com/kriolos>
+ * Copyright (C) 2022 KiolOS<https://github.com/kriolos>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
