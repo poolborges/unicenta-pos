d6e0c6d93334df2bbc00b44f5f73f20678f271e6
Paulo Borges <poolborges@hotmail.com>
2023-03-09 13:43:25 +0000

    Added initial POS REST server

---
+++ kriolos-opos-restserver/src/main/java/io/github/kriolos/opos/restserver/KriolosOposRestserverApplication.java
@@ -0,0 +1,13 @@
+package io.github.kriolos.opos.restserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KriolosOposRestserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KriolosOposRestserverApplication.class, args);
+	}
+
+}
