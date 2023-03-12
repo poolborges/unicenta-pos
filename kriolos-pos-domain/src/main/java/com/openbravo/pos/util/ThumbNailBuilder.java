84c52052f228acca05d86931611248e2e2778069
Paulo Borges <poolborges@hotmail.com>
2023-03-12 18:56:36 +0000

    FIX ThumbNailBuilder

--- kriolos-pos-domain/src/main/java/com/openbravo/pos/util/ThumbNailBuilder.java
+++ kriolos-pos-domain/src/main/java/com/openbravo/pos/util/ThumbNailBuilder.java
@@ -16,17 +16,21 @@
  */
 package com.openbravo.pos.util;
 
-import com.openbravo.data.loader.ImageUtils;
-import java.awt.image.*;
-import java.awt.*;
-import java.awt.font.TextAttribute;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GradientPaint;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Paint;
+import java.awt.Point;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
 import java.io.IOException;
-import java.net.URL;
-import java.text.AttributedString;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.JLabel;
-import net.coobird.thumbnailator.Thumbnailator;
 
 /**
  *
@@ -36,9 +40,15 @@ public class ThumbNailBuilder {
 
     private static final Logger LOGGER = Logger.getLogger(ThumbNailBuilder.class.getName());
 
-    private Image defaultImage;
-    private int thumbWidth;
-    private int thumbHeight;
+    private Image m_imgdefault;
+
+    private int m_width;
+
+    private int m_height;
+
+    public ThumbNailBuilder(int width, int height) {
+        init(width, height, null);
+    }
 
     public ThumbNailBuilder(int width, int height, Image imgdef) {
         init(width, height, imgdef);
@@ -46,127 +56,143 @@ public class ThumbNailBuilder {
 
     public ThumbNailBuilder(int width, int height, String img) {
         try {
-            URL imageUrl = getClass().getResource(img);
-
-            if (imageUrl == null) {
-                imageUrl = getClass().getClassLoader().getResource(img);
-                
-                if (imageUrl == null) {
-                    imageUrl = ClassLoader.getSystemResource(img);
-                }
-            }
-
-            if (imageUrl != null) {
-                init(width, height, ImageIO.read(imageUrl));
-            }
-
-        } catch (IOException fnfe) {
-            //TODO should log
+            init(width, height, ImageIO.read(getClass().getClassLoader().getResourceAsStream(img)));
+        } catch (IOException ex) {
+            LOGGER.log(Level.WARNING, "Exception creating t", ex);
+            init(width, height, null);
         }
-
     }
 
     private void init(int width, int height, Image imgdef) {
-        thumbWidth = width;
-        thumbHeight = height;
+        this.m_width = width;
+        this.m_height = height;
         if (imgdef == null) {
-            defaultImage = ImageUtils.generateColorImage(Color.WHITE, width, height);
+            this.m_imgdefault = null;
         } else {
-            defaultImage = createThumb(imgdef);
+            this.m_imgdefault = createThumbNail(imgdef);
         }
+    }
+
+    public int getWidth() {
+        return this.m_width;
+    }
+
+    public int getHeight() {
+        return this.m_height;
+    }
+
+    public Image getThumbNail(Image img) {
+        if (img == null) {
+            return this.m_imgdefault;
+        }
+        return createThumbNail(img);
     }
 
     public Image getThumbNail() {
-        return defaultImage;
-    }
-    
-    public Image getThumbNail(Image img) {
-        if(img != null)
-            return createThumb(img);
-        else 
-            return defaultImage;
+        return this.m_imgdefault;
     }
 
-    public Image getThumbNail(Image img, String text) {
-        if(img != null)
-            return createThumb(img, text);
-        else{
-            return createThumb(defaultImage, text);
-        }
-    }
-    
     public Image getThumbNailText(Image img, String text) {
-                
         img = getThumbNail(img);
-        
-        BufferedImage imgtext = new BufferedImage(img.getWidth(null), img.getHeight(null),  BufferedImage.TYPE_INT_ARGB);
+        BufferedImage imgtext = new BufferedImage(img.getWidth(null), img.getHeight(null), 2);
         Graphics2D g2d = imgtext.createGraphics();
-                
-        // The text        
         JLabel label = new JLabel();
-        Font font = label.getFont();
-        Font plainFont = new Font(font.getFontName(), Font.PLAIN,font.getSize());
-        label.setFont(plainFont);
         label.setOpaque(false);
         label.setText(text);
-        label.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        label.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);            
+        label.setHorizontalAlignment(0);
+        label.setVerticalAlignment(3);
         Dimension d = label.getPreferredSize();
         label.setBounds(0, 0, imgtext.getWidth(), d.height);
-        
-        // The background
-        Color c1 = new Color(0xff, 0xff, 0xff, 0x40);
-        Color c2 = new Color(0xff, 0xff, 0xff, 0xd0);
-
-        Paint gpaint = new GradientPaint(new Point(0,0), c1, new Point(label.getWidth() / 2, 0), c2, true);
-        
-        g2d.drawImage(img, 0, 0, null);
+        Color c1 = new Color(255, 255, 255, 64);
+        Color c2 = new Color(255, 255, 255, 208);
+        Paint gpaint = new GradientPaint(new Point(0, 0), c1, new Point(label.getWidth() / 2, 0), c2, true);
+        g2d.drawImage(img, 0, 0, (ImageObserver) null);
         g2d.translate(0, imgtext.getHeight() - label.getHeight());
-        g2d.setPaint(gpaint);            
-        g2d.fillRect(0 , 0, imgtext.getWidth(), label.getHeight());    
+        g2d.setPaint(gpaint);
+        g2d.fillRect(0, 0, imgtext.getWidth(), label.getHeight());
         label.paint(g2d);
-            
         g2d.dispose();
-        
-        return imgtext;    
+        return imgtext;
     }
 
-    
-    private Image createThumb(Image img, String text){
-        
-        BufferedImage imageBuf = ImageUtils.toBufferedImage(createThumb(img));
-
-        JLabel label = new JLabel();
-        Font font = label.getFont();
-        //Font font = new Font("Arial", Font.BOLD, 18);
-
-        AttributedString attributedText = new AttributedString(text);
-        attributedText.addAttribute(TextAttribute.FONT, font);
-        attributedText.addAttribute(TextAttribute.FOREGROUND, Color.BLACK);
-
-        Graphics g = imageBuf.getGraphics();
-
-        FontMetrics metrics = g.getFontMetrics(font);
-
-        //Center
-        //int positionX = (imageBuf.getWidth() - metrics.stringWidth(text)) / 2;
-        //int positionY = (imageBuf.getHeight() - metrics.getHeight()) / 2 + metrics.getAscent();
-        //Top Left
-        int positionX = 0;
-        int positionY = metrics.getAscent();
-
-        g.drawString(attributedText.getIterator(), positionX, positionY);
-
-        return imageBuf;
+    public Image getThumbNailText(BufferedImage imgBuff, String text) {
+        Image img = imgBuff.getScaledInstance(imgBuff.getWidth(), imgBuff.getHeight(), imgBuff.getType());
+        return getThumbNailText(img, text);
     }
 
-    private Image createThumb(Image img) {
+    public Image getThumbNail(BufferedImage imgBuff, String text) {
+        return getThumbNailText(imgBuff, text);
+    }
 
-        //Image is already in size or too small
-        if (img.getHeight(null) <= thumbHeight && img.getWidth(null) <= thumbWidth) {
-            return img;
+    private Image createThumbNail(Image img) {
+
+        int targetw;
+        int targeth;
+
+        double scalex = (double) m_width / (double) img.getWidth(null);
+        double scaley = (double) m_height / (double) img.getHeight(null);
+        if (scalex < scaley) {
+            targetw = m_width;
+            targeth = (int) (img.getHeight(null) * scalex);
+        } else {
+            targetw = (int) (img.getWidth(null) * scaley);
+            targeth = m_height;
         }
 
-        return Thumbnailator.createThumbnail(img, thumbWidth, thumbWidth);
+        int midw = img.getWidth(null);
+        int midh = img.getHeight(null);
+        BufferedImage midimg = null;
+        Graphics2D g2d = null;
+
+        Image previmg = img;
+        int prevw = img.getWidth(null);
+        int prevh = img.getHeight(null);
+
+        do {
+            if (midw > targetw) {
+                midw /= 2;
+                if (midw < targetw) {
+                    midw = targetw;
+                }
+            } else {
+                midw = targetw;
+            }
+            if (midh > targeth) {
+                midh /= 2;
+                if (midh < targeth) {
+                    midh = targeth;
+                }
+            } else {
+                midh = targeth;
+            }
+            if (midimg == null) {
+                midimg = new BufferedImage(midw, midh, BufferedImage.TYPE_INT_ARGB);
+                g2d = midimg.createGraphics();
+                g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+            }
+
+            if (g2d != null) {
+                g2d.drawImage(previmg, 0, 0, midw, midh, 0, 0, prevw, prevh, null);
+            }
+            prevw = midw;
+            prevh = midh;
+            previmg = midimg;
+        } while (midw != targetw || midh != targeth);
+
+        if (g2d != null) {
+            g2d.dispose();
+        }
+
+        if (m_width != midimg.getWidth() || m_height != midimg.getHeight()) {
+            midimg = new BufferedImage(m_width, m_height, BufferedImage.TYPE_INT_ARGB);
+            int x = (m_width > targetw) ? (m_width - targetw) / 2 : 0;
+            int y = (m_height > targeth) ? (m_height - targeth) / 2 : 0;
+            g2d = midimg.createGraphics();
+            g2d.drawImage(previmg, x, y, x + targetw, y + targeth,
+                    0, 0, targetw, targeth, null);
+            g2d.dispose();
+            previmg = midimg;
+        }
+        return previmg;
     }
 }
