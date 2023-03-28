8597bf2481577bc903cccde8f231734612743ae9
Paulo Borges <poolborges@hotmail.com>
2023-03-28 20:00:14 +0100

    Machine: update

--- kriolos-pos-machine/src/main/java/io/github/kriolos/opos/machine/KriolosPosMachine.java
+++ kriolos-pos-machine/src/main/java/io/github/kriolos/opos/machine/KriolosPosMachine.java
@@ -25,24 +25,24 @@ public class KriolosPosMachine {
         SystemInfo si = new SystemInfo();
         OperatingSystem os = si.getOperatingSystem();
         System.out.println(os);
-        
+
         HardwareAbstractionLayer hal = si.getHardware();
         CentralProcessor cpu = hal.getProcessor();
-        System.out.println(cpu.getLogicalProcessorCount() + " CPU(s):");
-        
+        System.out.println(" CPU(s): " + cpu.getLogicalProcessorCount());
+
         System.out.println("PhysicalProcessor");
         for (PhysicalProcessor pcpu : cpu.getPhysicalProcessors()) {
             System.out.println(" " + pcpu);
         }
-        
+
         System.out.println("LogicalProcessor");
         for (LogicalProcessor lcpu : cpu.getLogicalProcessors()) {
             System.out.println(" " + lcpu);
         }
-        
+
         ComputerSystem comp = hal.getComputerSystem();
-        System.out.println("HardwareUUID: "+comp.getHardwareUUID());
-        System.out.println("SerialNumber: "+comp.getSerialNumber());
+        System.out.println("HardwareUUID: " + comp.getHardwareUUID());
+        System.out.println("SerialNumber: " + comp.getSerialNumber());
 
         System.out.println("Memory: "
                 + FormatUtil.formatBytes(hal.getMemory().getAvailable()) + "/"
@@ -51,30 +51,28 @@ public class KriolosPosMachine {
         for (NetworkIF lcpu : hal.getNetworkIFs()) {
             System.out.println("MAC: " + lcpu.getMacaddr());
         }
-        
+
         Sensors sensores = hal.getSensors();
         System.out.println("CpuTemp: " + sensores.getCpuTemperature());
         System.out.println("CpuVolt: " + sensores.getCpuVoltage());
         //System.out.println("Cpu: " + sensores.get);
 
-        
-        System.out.println("getProcessorID: "+ cpu.getProcessorIdentifier().getProcessorID());
-        System.out.println("getIdentifier: "+ cpu.getProcessorIdentifier().getIdentifier());
-        
+        System.out.println("getProcessorID: " + cpu.getProcessorIdentifier().getProcessorID());
+        System.out.println("getIdentifier: " + cpu.getProcessorIdentifier().getIdentifier());
+
         NetworkParams ntP = os.getNetworkParams();
-        System.out.println("getHostName: "+ ntP.getHostName());
-        for(String name: ntP.getDnsServers()){
-            System.out.println("DnsServer: "+ name);
+        System.out.println("getHostName: " + ntP.getHostName());
+        for (String name : ntP.getDnsServers()) {
+            System.out.println("DnsServer: " + name);
         }
-        
-        for(String name: ntP.getDnsServers()){
-            System.out.println("DnsServer: "+ name);
-        }
-        
+
+        System.out.println("Network Gateway (IPv4): " + ntP.getIpv4DefaultGateway());
+        System.out.println("Network Gateway (IPv6): " + ntP.getIpv6DefaultGateway());
+
         Gson json = new Gson();
         //String content = json.toJson(os, OperatingSystem.class);
         //System.out.println(os);
         System.out.println(json.toJson(ntP, NetworkParams.class));
-        
+
     }
 }
