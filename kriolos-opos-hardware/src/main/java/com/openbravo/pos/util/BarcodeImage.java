d3b469de1997912843c45a5d7ac11c599d8a961a
Paulo Borges <poolborges@hotmail.com>
2025-09-27 01:07:12 +0100

    IMPOV Barcode

--- kriolos-opos-hardware/src/main/java/com/openbravo/pos/util/BarcodeImage.java
+++ kriolos-opos-hardware/src/main/java/com/openbravo/pos/util/BarcodeImage.java
@@ -20,7 +20,6 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
-import com.google.zxing.oned.*;
 import com.google.zxing.qrcode.QRCodeWriter;
 import java.awt.Color;
 import java.awt.Graphics2D;
