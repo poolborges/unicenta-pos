3547057024ce4292ad485eed888b452954df38be
Paulo Borges <poolborges@hotmail.com>
2025-10-01 03:08:31 +0100

    Format code

--- kriolos-opos-domain/src/main/java/com/openbravo/pos/ticket/UserInfo.java
+++ kriolos-opos-domain/src/main/java/com/openbravo/pos/ticket/UserInfo.java
@@ -27,30 +27,19 @@ import java.io.Serializable;
 public class UserInfo implements Serializable {
 
     private static final long serialVersionUID = 7537578737839L;
-    private String m_sId;
-    private String m_sName;
+    private final String id;
+    private final String name;
 
-    /** Creates a new instance of UserInfoBasic
-     * @param id
-     * @param name */
     public UserInfo(String id, String name) {
-        m_sId = id;
-        m_sName = name;
+        this.id = id;
+        this.name = name;
     }
 
-    /**
-     *
-     * @return
-     */
     public String getId() {
-        return m_sId;
+        return id;
     }
 
-    /**
-     *
-     * @return
-     */
     public String getName() {
-        return m_sName;
+        return name;
     }
 }
